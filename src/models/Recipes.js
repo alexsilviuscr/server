b6166b7b04fb8811bef55d2c39c32a2607c76181
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-12 00:14:44 +0300

    make it better

--- src/models/Recipes.js
+++ src/models/Recipes.js
@@ -1,23 +1,20 @@
 import mongoose from 'mongoose';
-import slugify from 'slugify';
 
 const RecipesSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
-  imageUrl: { type: String, required: true },
-  ingredients: [{ type: String, required: true }],
-  instructions: { type: String, required: true },
-  cookingTime: { type: Number, required: true },
-  userOwner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-    required: true
-  },
-});
-
-RecipesSchema.pre('save', function (next) {
-  this.slug = `${slugify(this.name, { lower: true, remove: /[*+~.()'"!:@]/g })}-${this._id}`;
-  next();
+    name: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    ingredients: [{ type: String, required: true }],
+    instructions: { type: String, required: true },
+    cookingTime: { type: Number, required: true },
+    userOwner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    // image: { 
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'image',
+    //     required: true },
 });
 
 export const RecipeModel = mongoose.model("recipes", RecipesSchema);
