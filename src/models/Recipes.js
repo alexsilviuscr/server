ae0aee648a2679249999b569d84876f1bc9653ee
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-12 11:04:56 +0300

    make it better

--- src/models/Recipes.js
+++ src/models/Recipes.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const RecipesSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    description: { type: String },
     slug: { type: String },
     imageUrl: { type: String, required: true },
     ingredients: [{ type: String, required: true }],
