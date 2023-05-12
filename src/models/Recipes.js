8665720cf24231b8ee1cb8fb8de4a837437c75b0
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-12 10:50:47 +0300

    make it better

--- src/models/Recipes.js
+++ src/models/Recipes.js
@@ -5,7 +5,7 @@ const RecipesSchema = new mongoose.Schema({
     slug: { type: String },
     imageUrl: { type: String, required: true },
     ingredients: [{ type: String, required: true }],
-    instructions: { type: String, required: true },
+    instructions: [{ type: String, required: true }],
     cookingTime: { type: Number, required: true },
     userOwner: {
         type: mongoose.Schema.Types.ObjectId,
