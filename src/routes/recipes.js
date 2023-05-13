5487cf90a0edf3647e4399100c31ecb3b90edf30
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-13 23:02:51 +0300

    make it better

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -113,17 +113,18 @@ router.delete("/:recipeId", async (req, res) => {
 });
 
 // remove saved recipe by id from saved recipes list
-router.delete("/saved-recipes/ids/:userId/:recipeId", async (req, res) => {
+router.delete("/saved-recipes/ids/:userId", async (req, res) => {
   try {
-      const user = await UserModel.findById(req.params.userId);
-      const updatedSavedRecipes = user.savedRecipes.filter((id) => id != req.params.recipeId);
-      user.savedRecipes = updatedSavedRecipes;
-      await user.save();
-      res.status(200).json({ message: "Recipe removed from saved recipes." });
-      console.log("Recipe removed from saved recipes");
+      const result = await RecipeModel.findByIdAndDelete(req.params.recipeId);
+      if (!result) {
+          res.status(404).json({ message: "Recipe not found." });
+          console.log("Recipe not found");
+      } else {
+          res.status(200).json({ message: "Recipe deleted." });
+          console.log("Recipe deleted");
+      }
   } catch (error) {
-      res.status(500).json({ message: "Couldn't remove recipe from saved recipes." });
-      console.log("Couldn't remove recipe from saved recipes");
+      res.status(500).json({ message: "Couldn't delete recipe." });
   }
 });
 
