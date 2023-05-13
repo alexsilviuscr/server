1c3683eb6ea6ee92f01588bdfd8e7e1ecd624d5d
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-13 15:06:12 +0300

    make it better

--- src/routes/users.js
+++ src/routes/users.js
@@ -59,4 +59,18 @@ router.post("/login", async (req, res) => {
     res.json({ token, userID: user._id });
 }); 
 
+router.get("/:id/username", async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        res.json({ username: user.username });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server Error." });
+    }
+});
+
+
 export { router as userRouter }; 
