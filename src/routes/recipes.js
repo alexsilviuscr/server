dbc5063032d9d3a9b13a93d2ded6cfaee3df774b
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-14 01:34:22 +0300

    make it better

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -112,7 +112,10 @@ router.get("/saved-recipes/:userId", async (req, res) => {
 //   }
 // });
 
-router.delete("/saved-recipes/:userId", async (req, res) => {
+// delete a recipe from the savedRecipes array
+
+router.delete("/saved-recipes/ids/:userId", async (req, res) => {
+
   try {
     const user = await UserModel.findById(req.params.userId);
 
